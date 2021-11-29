1928b75edabf66a169524644eef06525c5f5397e
Сергей Дробышев <serjdrob@gmail.com>
2021-11-29 10:41:51 +0700

    VTupleConverter is added

---
+++ Classes/CameraSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace ExposureMachine
+{
+    public class CameraSettings
+    {
+        public bool Monochrome { get; set; }
+        public int Brightness { get; set; }
+        public int Contrast { get; set; }
+        public int Saturation { get; set; }
+    }
+}
---
+++ View/Converters/VTupleConverter.cs
@@ -0,0 +1,32 @@
+﻿using ExposureMachine.Common;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ExposureMachine.View.Converters
+{
+    internal class VTupleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var tuple = ((Buttons, int))value;
+                return tuple.Item2;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
